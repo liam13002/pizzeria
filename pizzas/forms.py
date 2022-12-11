456be90b89ff03364bd2329e899ea8e05c8222f0
Liam McColgan <liam_mccolgan1@baylor.edu>
2022-12-11 14:53:23 -0600

    second push

---
+++ pizzas/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import *
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        labels = {'text':''}
+
+        widgets = {'text': forms.Textarea(attrs={'cols':80})}
